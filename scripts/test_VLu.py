3a666ad72b36af93b0bc91816818674a864f827f
Wu <jianan.w@compute1-client-1.ris.wustl.edu>
2026-01-16 12:41:12 -0600

    running layers

--- scripts/test_VLu.py
+++ scripts/test_VLu.py
@@ -22,13 +22,13 @@ plain_cif_path = project_root / 'data' / 'structures' / 'LFO_Orth2.cif'
 defect_cif_path = project_root / 'data' / 'structures' / f'LFO_Orth_{batch}.cif'
 layer_plain = ase.io.read(plain_cif_path)
 layer_defect = ase.io.read(defect_cif_path)
-save_dir = project_root / 'data' / 'simulations' / f'test_{batch}3'
+save_dir = project_root / 'data' / 'simulations' / f'test_{batch}'
 save_dir.mkdir(parents=True, exist_ok=True)
 
 # Set simulation on GPU
 abtem.config.set({"device": "gpu", "fft": "fftw"})
 
-layers = 5 # total number of layers in the simulation
+layers_list = [5, 10, 15, 20, 25, 30, 35, 40]
 energy = 100e3  # eV
 Cs = 0          # in Angstroms
 semiangle_cutoff = 30  # in milliradians
@@ -74,14 +74,20 @@ def run_simulation(layer_plain, layer_defect, current_layer_index, total_layers)
     
     # Post-processing
     haadf = flexible_measurement.integrate_radial(80, 200)
-    output = haadf.interpolate(0.05).gaussian_filter(0.3).poisson_noise(dose_per_area=1e7).array.T
-    filename = f"{batch}_layer{current_layer_index}.npy"
+    output = haadf.interpolate(0.05).gaussian_filter(0.3).array.T
+    output_noise = haadf.interpolate(0.05).gaussian_filter(0.3).poisson_noise(dose_per_area=1e7).array.T
+    filename = f"{batch}_layer{current_layer_index}_total{total_layers}.npy"
+    filename_noise = f"{batch}_layer{current_layer_index}_total{total_layers}_noise.npy"
     np.save(save_dir / filename, output)
+    np.save(save_dir / filename_noise, output_noise)
     return output
 
-print(f"Starting batch {batch} for {layers} layers...")
+print(f"Starting batch {batch} for thicknesses {layers_list}...")
 
-for i in tqdm(range(layers), desc="Simulating Layers"):
-    run_simulation(layer_plain, layer_defect, i, layers)
+for total_layers in layers_list:
+    print(f"\n--- Starting simulation for Total Layers: {total_layers} ---")
+
+    for i in tqdm(range(total_layers), desc=f"Layers (Total={total_layers})"):
+        run_simulation(layer_plain, layer_defect, i, total_layers)
 
 print("All simulations complete.")
